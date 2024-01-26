50fe0c86e634db3a83c73465915cb7ad4534974c
Александр Казаков <29281112@s.mfua.ru>
2024-01-26 04:25:33 +0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import svgr from "vite-plugin-svgr"
 /// <reference types="vite-plugin-svgr/client" />
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cyber-samurai',
   plugins: [
       react(),
       svgr()
