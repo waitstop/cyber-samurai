79bb998ff88919642d6b328dca5ccf963c03b856
Aleksandr waitstop Kazakov <kazakov2003931@yandex.ru>
2024-02-05 18:58:19 +0300

    feat(\): header adaptive

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,36 +9,50 @@ import {
 } from "@components/ui/sheet.tsx";
 
 const Header = () => {
+  const navItems: { name: string; href: string; className?: string }[] = [
+    { name: "Что мы делаем", href: "#" },
+    { name: "Наши инструменты", href: "#" },
+    { name: "Кейсы", href: "#" },
+    { name: "Ваша боль/наше решение", href: "#" },
+    {
+      name: "Связаться с нами",
+      href: "#",
+      className: "button w-fit bg-white text-zinc-800",
+    },
+  ];
   return (
     <header className={"sticky top-0 z-50 bg-violet-accent p-2.5"}>
       <div className={"container flex items-center justify-between"}>
         <Link to={"/"}>
           <LogoIcon />
         </Link>
+        <ul
+          className={
+            "hidden items-center justify-center gap-5 text-white lg:flex"
+          }
+        >
+          {navItems.map((item, index) => (
+            <li key={index}>
+              <a className={item.className} href={item.href}>
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
         <Sheet>
-          <SheetTrigger>
+          <SheetTrigger className={"lg:hidden"}>
             <BurgerIcon />
           </SheetTrigger>
-          <SheetContent>
+          <SheetContent className={"lg:hidden"}>
             <SheetDescription>
               <ul className={"[&>*:not(:last-child)]:mb-5"}>
-                <li>
-                  <a href="#">Что мы делаем</a>
-                </li>
-                <li>
-                  <a href="#">Наши инструменты</a>
-                </li>
-                <li>
-                  <a href="#">Кейсы</a>
-                </li>
-                <li>
-                  <a href="#">Ваша боль/наше решение</a>
-                </li>
-                <li>
-                  <a className={"button w-fit bg-white text-zinc-800"} href="#">
-                    Связаться с нами
-                  </a>
-                </li>
+                {navItems.map((item, index) => (
+                  <li key={index}>
+                    <a className={item.className} href={item.href}>
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
               </ul>
             </SheetDescription>
           </SheetContent>
