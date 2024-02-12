eaa5c175e86122c2f4bad0e3d57d04117860414b
Александр Казаков <29281112@s.mfua.ru>
2024-02-12 08:45:49 +0300

    feat: header change color depends of page

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,29 +1,52 @@
 import LogoIcon from "@icons/logo.svg?react";
 import BurgerIcon from "@icons/burger.svg?react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 import {
   Sheet,
   SheetContent,
   SheetDescription,
   SheetTrigger,
 } from "@components/ui/sheet.tsx";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils.ts";
+import { routesPaths } from "@providers/router/routesPaths.ts";
 
 const Header = () => {
-  const navItems: { name: string; href: string; className?: string }[] = [
+  const navItems: {
+    name: string;
+    href: routesPaths | string;
+    className?: string;
+  }[] = [
     { name: "Что мы делаем", href: "#" },
     { name: "Наши инструменты", href: "#" },
     { name: "Кейсы", href: "#" },
     { name: "Ваша боль/наше решение", href: "#" },
     {
       name: "Связаться с нами",
-      href: "#",
+      href: "#contact",
       className: "button w-fit bg-white text-zinc-800",
     },
   ];
-
+  const { pathname } = useLocation();
+  useEffect(() => {
+    if (pathname === routesPaths.ROADMAP) {
+      setHeaderBg("bg-blue-700");
+    }
+    if (pathname === routesPaths.TECHNOLOGIES) {
+      setHeaderBg("bg-red-500");
+    }
+    if (pathname === routesPaths.SOLUTIONS) {
+      setHeaderBg("bg-amber-400");
+    }
+  }, [pathname]);
+  const [isSheetOpen, setIsSheetOpen] = useState<boolean>(false);
+  const [headerBg, setHeaderBg] = useState<string>("");
   return (
-    <header className={"sticky top-0 z-50 bg-violet-accent p-2.5"}>
-      <div className={"container flex items-center justify-between"}>
+    <header
+      className={cn("sticky top-0 z-50 h-16 bg-violet-accent p-2.5", headerBg)}
+    >
+      <div className={"container flex h-full items-center justify-between"}>
         <Link to={"/"}>
           <LogoIcon />
         </Link>
@@ -33,14 +56,24 @@ const Header = () => {
           }
         >
           {navItems.map((item, index) => (
-            <li key={index}>
+            <li
+              className={
+                !item.className
+                  ? "relative before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-center before:scale-0 before:bg-white before:opacity-0 before:transition-all hover:before:scale-100 hover:before:opacity-100"
+                  : ""
+              }
+              key={index}
+            >
               <a className={item.className} href={item.href}>
                 {item.name}
               </a>
             </li>
           ))}
         </ul>
-        <Sheet>
+        <Sheet
+          onOpenChange={(state) => setIsSheetOpen(state)}
+          open={isSheetOpen}
+        >
           <SheetTrigger className={"lg:hidden"}>
             <BurgerIcon />
           </SheetTrigger>
@@ -49,9 +82,23 @@ const Header = () => {
               <ul className={"[&>*:not(:last-child)]:mb-5"}>
                 {navItems.map((item, index) => (
                   <li key={index}>
-                    <a className={item.className} href={item.href}>
-                      {item.name}
-                    </a>
+                    {item.href[0] === "#" ? (
+                      <HashLink
+                        onClick={() => setIsSheetOpen(false)}
+                        className={item.className}
+                        to={item.href}
+                      >
+                        {item.name}
+                      </HashLink>
+                    ) : (
+                      <Link
+                        onClick={() => setIsSheetOpen(false)}
+                        className={item.className}
+                        to={item.href}
+                      >
+                        {item.name}
+                      </Link>
+                    )}
                   </li>
                 ))}
               </ul>
